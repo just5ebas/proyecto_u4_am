9b9eec8b676a1584dd2045e4998f7bc8796fcaee
Ariel Maldonado <arielsebas2000@gmail.com>
2022-09-02 20:35:15 -0500

    Programacion WEB, primera clase.

--- src/main/java/com/uce/edu/demo/repository/PersonaJpaRepositoryImpl.java
+++ src/main/java/com/uce/edu/demo/repository/PersonaJpaRepositoryImpl.java
@@ -4,19 +4,12 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 import com.uce.edu.demo.repository.modelo.Persona;
-import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
-import com.uce.edu.demo.repository.modelo.PersonaSencilla;
 
 @Repository
 @Transactional
